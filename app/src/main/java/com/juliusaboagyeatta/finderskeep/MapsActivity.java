fdd72a6ce4bba5c435609bb60adea15d3764bef5
Julius Aboagye-Atta <juliusaboagyeatta@rocketmail.com>
2017-02-23 11:29:07 +0000

    test on new demos

--- app/src/main/java/com/juliusaboagyeatta/finderskeep/MapsActivity.java
+++ app/src/main/java/com/juliusaboagyeatta/finderskeep/MapsActivity.java
@@ -7,6 +7,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import android.location.Location;
@@ -34,6 +35,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        if (mMap != null) {
+            mMap.setMyLocationEnabled(true);
+        }
     }
 
 
